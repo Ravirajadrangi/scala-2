fd1bd39862a574c146f959275d948b9e13324fa1
Filipe Almeida <filipe.lmd@gmail.com>
2015-08-06 08:07:55 +0100

    first steps with scala:
    - Basic IntelliJ idea run configurations (going through application instead of scala console)
    
    - Hello World

--- ScalaDemo/src/demo.scala
+++ ScalaDemo/src/demo.scala
@@ -1,12 +1,24 @@
 object demo {
-  def main (args: Array[String]) {
-    val maxRange: Int = 50
+
+  def printHello(maxRange : Int): Unit ={
     val helloWorld : String = "Hello World"
-    val numbers: Range = 1.to(maxRange)
+    val numbers : Range = 1.to(maxRange)
+
+    var oddEven : String = null
 
     for(i <- numbers){
-      println(s"$helloWorld $i")
-    }
+      oddEven =
+        if(i % 2 == 0)
+          "Even"
+        else
+          "Odd"
 
+      println(s"$oddEven - $helloWorld $i")
+    }
+  }
+
+  def main (args: Array[String]) {
+    val maxRange: Int = 20
+    printHello(maxRange)
   }
 }
